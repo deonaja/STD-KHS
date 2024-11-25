9e5a578759153d985ba07f087c61c83b0fea3a5a
deonaja <deonpwa@gmail.com>
2024-11-26 02:37:02 +0700

    apa

---
+++ TUBES/Tubes kece/tubes.h
@@ -0,0 +1,59 @@
+#ifndef TUBES_H_INCLUDED
+#define TUBES_H_INCLUDED
+#include <iostream>
+
+using namespace std;
+
+typedef struct elemenGudang* adrGudang;
+typedef struct elemenPenitip* adrPenitip;
+typedef struct elemenRelasi* adrRelasi;
+
+struct gudang {
+    int nomor_gudang;
+    int slot_tersedia_gudang;
+};
+
+struct penitip {
+    string nama_penitip;
+    string tanggal_masuk_barang;
+    int jumlah_barang;
+    string[] info_barang;
+};
+
+struct elemenRelasi {
+    adrRelasi nextRelasi;
+    adrPenitip nextPenitip;
+};
+
+struct elemenGudang {
+    gudang info;
+    adrRelasi nextRelasi;
+    adrGudang next;
+};
+
+struct elemenPenitip {
+    penitip info;
+    adrPenitip next;
+    adrPenitip prev;
+};
+
+struct listRelasi {
+    adrRelasi first;
+};
+
+struct listPenitip {
+    adrPenitip first;
+    adrPenitip last;
+};
+
+struct listGudang {
+    adrGudang first;
+};
+
+void menu();
+void insertGudang();
+void insertPenitip();
+void insertRelation(listGudang &lG, adrGudang G, adrPenitip P); //G untuk data parent yang ingin ditambah relasinya; P untuk child yang akan direlasikan ke parent
+
+
+#endif // TUBES_H_INCLUDED
