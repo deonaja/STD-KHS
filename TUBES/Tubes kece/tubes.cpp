41e6adec49904b6eabd48e081160305b81a7e51a
Karina Azzahra <157381543+kawreenazzhra@users.noreply.github.com>
2024-11-27 02:15:13 +0700

    Update tubes.cpp

--- TUBES/Tubes kece/tubes.cpp
+++ TUBES/Tubes kece/tubes.cpp
@@ -1,2 +1,18 @@
 #include <tubes.h>
 
+void insertGudang(listGudang &lG, adrGudang G){
+    G = new elemenGudang;
+    G->info.nomor_gudang = x.nomor_gudang;
+    G->info.slot_tersedia_gudang = x.slot_tersedia_gudang;
+    G->next = NULL;
+    if (lG.first==NULL){
+        lG.first = G;
+    }else{
+        adrParent Q;
+        Q = lG.first;
+        while (Q->next!=NULL){
+        Q = Q->next;
+        }
+        Q->next = G;
+    }
+}
